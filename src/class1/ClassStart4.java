e74d25421af9c3d4642176340478f2d745bb2a1a
RyuJaeJeong <fb0982@naver.com>
2024-03-04 23:10:01 +0900

    클래스에 배열을 추가한 경우

---
+++ src/class1/ClassStart4.java
@@ -0,0 +1,24 @@
+package class1;
+
+public class ClassStart4 {
+    
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.name = "학생1";
+        student1.age = 15;
+        student1.grade = 90;
+
+        Student student2 = new Student();
+        student2.name = "학생2";
+        student2.age = 16;
+        student2.grade = 80;
+
+        Student[] students = new Student[2];
+        students[0] =  student1;
+        students[1] =  student2;
+
+        System.out.printf("이름: [%S] 나이: [%d] 성적: [%d]\n", students[0].name, students[0].age, students[0].grade);
+        System.out.printf("이름: [%S] 나이: [%d] 성적: [%d]\n", students[1].name, students[1].age, students[1].grade);
+
+    }
+}
